0e4ae1bb60e9a736a2123739cfbce2dc5a0f777c
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-07 16:50:12 +0200

    wantTOknow

---
+++ components/all/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="rm-container bg-white py-16 px-8">
+    <img
+      src="public/img/wellness.png"
+      alt="Filigrane"
+      class="absolute top-0 left-0 opacity-20 w-32 h-32 z-10"
+    />
+    <!-- Upper Section Title -->
+    <p
+      class="text-yellow-500 text-2xl font-semibold flex items-center justify-center text-center"
+    >
+      <span class="inline-block w-8 h-0.5 bg-yellow-500 mr-3"></span>
+      <span
+        class="transform transition duration-200 hover:scale-105 font-title"
+      >
+        {{ menu }}
+      </span>
+    </p>
+    <h1
+      class="text-center text-3xl font-bold mb-12 transform transition duration-400 hover:scale-105"
+    >
+      {{ menu }}
+    </h1>
+  </section>
+</template>
+
+<script setup>
+const { t } = useI18n();
+const menu = ref(t("menu.news"));
+const localPath = useLocalePath();
+</script>
---
+++ pages/news.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <div class="rm-wrapper">
+      <all />
+    </div>
+  </div>
+</template>
