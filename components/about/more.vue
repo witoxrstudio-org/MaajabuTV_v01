6a53f01bc51b2f6a5d0353efb39f910c7bafc60e
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-07 15:22:00 +0200

    WeGoon

--- components/about/more.vue
+++ components/about/more.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="rm-container bg-white -pt-8 px-8 sm:px-6 md:px-8 relative">
+  <section class="rm-container bg-gray-100 -pt-8 px-8 sm:px-6 md:px-8 relative">
     <!-- Upper Section Title -->
     <img
       src="public/img/Titik-titik.png"
@@ -25,7 +25,7 @@
       </h1>
 
       <p
-        class="text-base sm:text-lg text-black leading-relaxed text-center px-4 sm:px-0"
+        class="text-black leading-relaxed text-center px-4 sm:px-0 transform transition duration-500 hover:scale-105 hover:underline"
       >
         {{ three }}
       </p>
@@ -41,9 +41,7 @@
             <img src="public/img/z1.png" alt="Trophy" class="w-12 h-auto" />
           </div>
           <h2 class="font-semibold text-lg">{{ about_icon_1 }}</h2>
-          <p>
-            {{ about_icon_desc1 }}
-          </p>
+          <p>{{ about_icon_desc1 }}</p>
         </div>
         <div
           class="flex flex-col items-center text-center transform transition duration-200 hover:scale-105 hover:underline"
@@ -52,44 +50,16 @@
             <img src="public/img/z2.png" alt="Trophy" class="w-12 h-auto" />
           </div>
           <h2 class="font-semibold text-lg">{{ about_icon_2 }}</h2>
-          <p>
-            {{ about_icon_desc2 }}
-          </p>
+          <p>{{ about_icon_desc2 }}</p>
         </div>
       </div>
 
-      <!-- Carte Centrale -->
-      <div class="relative">
-        <div
-          class="bg-cover bg-center relative rounded-lg overflow-hidden shadow-lg"
-          id="card"
-        >
-          <!-- Dégradé -->
-          <div
-            class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"
-          ></div>
-
-          <!-- Icone en haut à droite -->
-          <div
-            class="absolute top-4 right-4 bg-yellow-500 text-white p-2 rounded-full"
-          >
-            🎶
-          </div>
-
-          <!-- Contenu de l'image (Titre et Auteur) -->
-          <div class="absolute bottom-4 left-4 text-white">
-            <h3 class="text-3xl font-bold">HakuniAcha</h3>
-            <p class="text-sm">Maajabu Gospel</p>
-            <p class="text-xl font-bold">Faveur Mukoko</p>
-          </div>
-        </div>
-
-        <!-- Image source -->
+      <!-- Image Centrale -->
+      <div class="flex items-center justify-center">
         <img
           src="public/img/fav.png"
-          alt="Carte image"
-          class="hidden"
-          id="imgSource"
+          alt="Image Centrale"
+          class="w-auto h-80 shadow-lg transform transition duration-300 hover:scale-105"
         />
       </div>
 
@@ -111,11 +81,29 @@
             <img src="public/img/z4.png" alt="Trophy" class="w-12 h-auto" />
           </div>
           <h2 class="font-semibold text-lg">{{ about_icon_4 }}</h2>
-          <p>
-            {{ about_icon_desc4 }}
-          </p>
+          <p>{{ about_icon_desc4 }}</p>
         </div>
       </div>
+    </div>
+    <div
+      class="flex flex-col justify-center md:flex-row md:space-x-4 mt-4 space-y-2 md:space-y-0"
+    >
+      <button
+        class="bg-yellow-500 hover:bg-yellow-600 text-black py-2 px-3 rounded-full font-semibold text-sm flex items-center justify-center space-x-2 transform transition duration-200 hover:scale-105 hover:text-green-500 w-full md:w-auto"
+      >
+        <i class="fab fa-spotify"></i> <span>Spotify</span>
+      </button>
+      <button
+        class="bg-yellow-500 hover:bg-yellow-600 text-black py-2 px-3 rounded-full font-semibold text-sm flex items-center justify-center space-x-2 transform transition duration-200 hover:scale-105 hover:text-orange-200 w-full md:w-auto"
+      >
+        <i class="fab fa-soundcloud"></i>
+        <span>SoundCloud</span>
+      </button>
+      <button
+        class="bg-yellow-500 hover:bg-yellow-600 text-black py-2 px-3 rounded-full font-semibold text-sm flex items-center justify-center space-x-2 transform transition duration-200 hover:scale-105 hover:text-white w-full md:w-auto"
+      >
+        <i class="fab fa-apple"></i> <span>Apple Podcast</span>
+      </button>
     </div>
   </section>
 </template>
