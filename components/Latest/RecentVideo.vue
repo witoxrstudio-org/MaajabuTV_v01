5bf5b3aba17043e5d3bcf35c99f4e57586e3ede5
vincejs77 <vinjobs77@gmail.com>
2024-11-11 22:22:08 +0200

    update(11-11-2024) 005

--- components/Latest/RecentVideo.vue
+++ components/Latest/RecentVideo.vue
@@ -1,58 +1,66 @@
 <template>
-    <div>
-        <a href="javascript:void(0)">
-            <div class="relative flex flex-col my-2 rounded-lg">
-                <div class="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
-                    <iframe class="w-full h-full" :src="videoUrl" :title="videoTitle" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                </div>
-                <div class="p-3">
-                    <!-- <div
-                        class="mb-4 rounded-full bg-yellow-400 py-0.3 px-2.2 border border-transparent text-xs text-white transition-all shadow-sm w-20 text-center">
+  <div>
+    <a href="javascript:void(0)">
+      <div class="relative my-2 flex flex-col rounded-lg">
+        <div class="relative h-56 overflow-hidden rounded-md text-white">
+          <iframe
+            class="h-full w-full"
+            :src="videoUrl"
+            :title="videoTitle"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div class="p-3">
+          <!-- <div
+                        class="py-0.3 px-2.2 mb-4 w-20 rounded-full border border-transparent bg-yellow-400 text-center text-xs text-white shadow-sm transition-all">
                         Popular
                     </div> -->
-                    <h6 class="mb-1 text-light-800 text-xl font-semibold">
-                        {{ videoTitle }}
-                    </h6>
-                </div>
+          <h6 class="text-light-800 mb-1 text-xl font-semibold">
+            {{ videoTitle }}
+          </h6>
+        </div>
 
-                <div class="flex items-center justify-between p-3">
-                    <div class="flex items-center">
-                        <img :alt="videoArtist" :src="videoPhoto" class="relative inline-block h-8 w-8 rounded-full" />
-                        <div class="flex flex-col ml-3 text-sm">
-                            <span class="text-light-800 font-semibold">{{ videoArtist }}</span>
-                            <span class="text-yellow-400">
-                                Artist
-                            </span>
-                        </div>
-                    </div>
-                </div>
+        <div class="flex items-center justify-between p-3">
+          <div class="flex items-center">
+            <img
+              :alt="videoArtist"
+              :src="videoPhoto"
+              class="relative inline-block h-8 w-8 rounded-full"
+            />
+            <div class="ml-3 flex flex-col text-sm">
+              <span class="text-light-800 font-semibold">{{ videoArtist }}</span>
+              <span class="text-yellow-400"> Artist </span>
             </div>
-        </a>
-    </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'RecentVideo',
-    props: {
-        videoUrl: {
-            type: String,
-            required: true
-        },
-        videoTitle: {
-            type: String,
-            required: true
-        },
-        videoPhoto: {
-            type: String,
-            required: true
-        },
-        videoArtist: {
-            type: String,
-            required: true
-        }
-    }
+  name: "RecentVideo",
+  props: {
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    videoTitle: {
+      type: String,
+      required: true,
+    },
+    videoPhoto: {
+      type: String,
+      required: true,
+    },
+    videoArtist: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
--- pages/tv.vue
+++ pages/tv.vue
@@ -1,102 +1,93 @@
 <template>
   <div>
-    <section class="bg-black text-white flex items-center justify-center pt-16">
-      <div class="mj-container border-r border-l border-slate-700">
-        <h1 class="text-4xl font-bold py-10 text-center">
+    <section class="flex items-center justify-center bg-black pt-16 text-white">
+      <div class="mj-container border-slate-700 pt-16">
+        <h1 class="pb-4 text-center text-sm font-bold sm:text-xl">
           <i class="fa-solid fa-tv"></i>TV
         </h1>
 
-        <div class="flex flex-col items-center justify-center space-y-6">
+        <div class="flex w-full flex-col items-center justify-center space-y-6">
           <!-- Titre principal de la page -->
-          <h1 class="text-3xl font-bold text-yellow-600 hover:text-yellow-500">
-            En Direct - Vidéo la Plus Récente
+          <h1
+            class="text-2xl font-bold text-yellow-600 hover:text-yellow-500 sm:text-4xl"
+          >
+            En direct
           </h1>
 
           <!-- Description de la vidéo -->
-          <p class="text-white max-w-2xl text-center hover:text-white">
-            Regardez le dernier enregistrement live de "Mungu wa Maajabu"
-            interprété par Deborah Lukalu, avec la participation de Mike
-            Kalambay. Profitez de ce moment spirituel inspirant !
+          <p class="hidden max-w-2xl text-center text-white hover:text-white">
+            Regardez le dernier enregistrement live de "Mungu wa Maajabu" interprété par
+            Deborah Lukalu, avec la participation de Mike Kalambay. Profitez de ce moment
+            spirituel inspirant !
           </p>
 
-          <div
-            class="w-full max-w-4xl border border-yellow-600 shadow-lg overflow-hidden"
-          >
-            <div class="h-[80vh] w-full bg-gray-900 relative">
+          <div class="w-full overflow-hidden">
+            <div class="relative h-[300px] w-full bg-gray-900 sm:h-[500px]">
               <LatestLiveVideo
                 class="h-full w-full"
                 :videoUrl="'https://www.youtube.com/embed/8rhNPSmj0_c'"
                 :videoTitle="'Live Recording MUNGU WA MAAJABU - Mungu wa Maajabu by Deborah Lukalu feat Mike Kalambay'"
               />
+            </div>
+            <!-- Overlay du titre de la vidéo -->
+            <div class="left-0 w-full bg-gradient-to-t from-black to-transparent">
+              <h3 class="hidden pt-2 text-lg font-semibold text-white">
+                Live Recording MUNGU WA MAAJABU - Mungu wa Maajabu by Deborah Lukalu feat
+                Mike Kalambay
+              </h3>
 
-              <!-- Overlay du titre de la vidéo -->
+              <!-- Statistiques de la vidéo -->
               <div
-                class="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent p-4"
+                class="mt-3 flex justify-center space-x-4 rounded p-4 sm:grid sm:grid-cols-4 sm:space-x-2 md:flex md:justify-start md:space-x-4 md:text-left"
               >
-                <h3 class="text-white font-semibold pt-2 text-lg">
-                  Live Recording MUNGU WA MAAJABU - Mungu wa Maajabu by Deborah
-                  Lukalu feat Mike Kalambay
-                </h3>
+                <div class="flex items-center space-x-2">
+                  <i
+                    class="fas fa-headphones h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-yellow-500"
+                  ></i>
+                  <span class="text-xs sm:text-sm">8.9K</span>
+                </div>
 
-                <!-- Statistiques de la vidéo -->
-                <div
-                  class="mt-3 flex justify-center space-x-4 bg-yellow-600 p-4 rounded text-black sm:grid sm:grid-cols-4 sm:space-x-2 md:flex md:justify-start md:space-x-4 md:text-left"
-                >
-                  <div class="flex items-center space-x-2">
-                    <i
-                      class="fas fa-headphones h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-yellow-500"
-                    ></i>
-                    <span class="text-xs sm:text-sm">8.9K</span>
-                  </div>
+                <div class="flex items-center space-x-2">
+                  <i
+                    class="fas fa-heart h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-red-500"
+                  ></i>
+                  <span class="text-xs sm:text-sm">2.1K</span>
+                </div>
 
-                  <div class="flex items-center space-x-2">
-                    <i
-                      class="fas fa-heart h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-red-500"
-                    ></i>
-                    <span class="text-xs sm:text-sm">2.1K</span>
-                  </div>
+                <div class="flex items-center space-x-2">
+                  <i
+                    class="fas fa-comment h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-blue-500"
+                  ></i>
+                  <span class="text-xs sm:text-sm">982K</span>
+                </div>
 
-                  <div class="flex items-center space-x-2">
-                    <i
-                      class="fas fa-comment h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-blue-500"
-                    ></i>
-                    <span class="text-xs sm:text-sm">982K</span>
-                  </div>
-
-                  <div class="flex items-center space-x-2">
-                    <i
-                      class="fas fa-download h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-green-500"
-                    ></i>
-                    <span class="text-xs sm:text-sm">284K</span>
-                  </div>
+                <div class="flex items-center space-x-2">
+                  <i
+                    class="fas fa-download h-5 w-5 transition-transform duration-300 hover:scale-110 hover:text-green-500"
+                  ></i>
+                  <span class="text-xs sm:text-sm">284K</span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <hr class="mt-8 border-yellow-600" />
-        <div class="container mb-5 mt-4">
+        <hr class="mt-8 border-yellow-600 border-opacity-50" />
+        <div class="py-16">
           <h3
-            class="text-2xl sm:text-3xl md:text-4xl font-semibold text-center transition-all duration-300"
+            class="mb-8 text-center text-2xl font-semibold transition-all duration-300 sm:text-3xl md:text-4xl"
           >
             Recents Lives
           </h3>
 
-          <div class="container mx-auto p-6">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-              <div
-                v-for="row in recommandations"
-                :key="row.id"
-                class="flex justify-center"
-              >
-                <LatestRecentVideo
-                  :videoTitle="row.title"
-                  :videoUrl="row.link"
-                  :videoPhoto="row.photo"
-                  :videoArtist="row.artist"
-                  class="w-full max-w-sm h-full rounded-lg shadow-lg border border-transparent transition duration-300 hover:scale-105 hover:border-yellow-400 hover:shadow-2xl p-4 flex flex-col justify-between"
-                />
-              </div>
+          <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+            <div v-for="row in recommandations" :key="row.id" class="flex justify-center">
+              <LatestRecentVideo
+                :videoTitle="row.title"
+                :videoUrl="row.link"
+                :videoPhoto="row.photo"
+                :videoArtist="row.artist"
+                class="flex h-full w-full max-w-sm flex-col justify-between"
+              />
             </div>
           </div>
         </div>
@@ -120,8 +111,7 @@ const recommandations = ref([
     artist: "Maajabu Gospel",
     photo:
       "https://yt3.googleusercontent.com/ytc/AIdro_me7D1cYywCcsR-m_yijRZTOwx9h3oAk-fpareYt8nbBA=s160-c-k-c0x00ffffff-no-rj",
-    title:
-      "Maajabu Talent Europe - Demi-Finale - Old School - RUTH DANIEL - Saison 2",
+    title: "Maajabu Talent Europe - Demi-Finale - Old School - RUTH DANIEL - Saison 2",
     link: "https://www.youtube.com/embed/Q4VCeBUeWRI",
     stats: {
       listen: "8.9K",
