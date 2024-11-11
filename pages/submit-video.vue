cf7d8e28925f5d8cc01ef91ffa7f29548326b052
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-11-11 13:45:00 +0200

    SubmitTranslate

--- pages/submit-video.vue
+++ pages/submit-video.vue
@@ -15,28 +15,31 @@
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
               for="prenom"
-              >Prénom *</label
+            >
+              {{ prenomLabel }}</label
             >
             <input
               v-model="formData.prenom"
               type="text"
               id="prenom"
               required
-              placeholder="Veuillez saisir votre prénom"
+              :placeholder="prenomPlaceholder"
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             />
           </div>
 
           <div>
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="nom"
-              >Nom de famille *</label
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="nom"
+              >{{ nomLabel }}</label
             >
             <input
               v-model="formData.nom"
               type="text"
               id="nom"
               required
-              placeholder="Veuillez saisir votre nom de famille"
+              :placeholder="nomPlaceholder"
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             />
           </div>
@@ -47,17 +50,18 @@
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
               for="gender"
-              >Qui es-tu ?</label
             >
+              {{ genreLabel }}
+            </label>
             <select
               v-model="formData.gender"
               id="gender"
               required
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             >
-              <option value="Homme">Homme</option>
-              <option value="Femme">Femme</option>
-              <option value="Autre">Autre</option>
+              <option value="Homme">{{ genreOptionM }}</option>
+              <option value="Femme">{{ genreOptionF }}</option>
+              <option value="Autre">{{ genreOptionO }}</option>
             </select>
           </div>
 
@@ -65,14 +69,15 @@
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
               for="country"
-              >Pays *</label
             >
+              {{ countryLabel }}
+            </label>
             <input
               v-model="formData.country"
               type="text"
               id="country"
               required
-              placeholder="Veuillez saisir votre pays"
+              :placeholder="countryPlaceholder"
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             />
           </div>
@@ -83,7 +88,8 @@
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
               for="email"
-              >Adresse E-mail *</label
+            >
+              {{ emailLabel }}</label
             >
             <input
               v-model="formData.email"
@@ -91,11 +97,11 @@
               id="email"
               :class="{ 'border-red-500': !isEmailValid }"
               required
-              placeholder="Veuillez saisir votre email"
+              :placeholder="emailPlaceholder"
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             />
-            <span v-if="!isEmailValid" class="text-red-500 text-xs"
-              >Email invalide</span
+            <span v-if="!isEmailValid" class="text-red-500 text-xs">
+              {{ emailError }}</span
             >
           </div>
 
@@ -103,7 +109,7 @@
             <label
               class="block text-gray-700 text-sm font-bold mb-2"
               for="phone"
-              >Téléphone *</label
+              >{{ phoneLabel }}</label
             >
             <input
               v-model="formData.phone"
@@ -111,33 +117,36 @@
               id="phone"
               :class="{ 'border-red-500': !isPhoneValid }"
               required
-              placeholder="Veuillez entrer votre numéro de téléphone"
+              :placeholder="phonePlaceholder"
               class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
             />
-            <span v-if="!isPhoneValid" class="text-red-500 text-xs"
-              >Numéro de téléphone invalide</span
-            >
+            <span v-if="!isPhoneValid" class="text-red-500 text-xs">
+              {{ phoneError }}
+            </span>
           </div>
         </div>
 
         <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="social"
-            >Profile Social media *</label
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="social"
+          >
+            {{ socialLabel }}</label
           >
           <input
             v-model="formData.social"
             type="url"
             id="social"
             required
-            placeholder="Veuillez entrer votre lien de réseau social"
+            :placeholder="socialPlaceholder"
             class="border w-full py-2 px-3 text-gray-700 focus:outline-none focus:border-yellow-600"
           />
         </div>
 
         <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="image"
-            >Image de l'artiste *</label
-          >
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="image">
+            {{ imageLabel }}
+          </label>
           <input
             type="file"
             id="image"
@@ -152,7 +161,7 @@
             class="bg-yellow-600 w-full hover:bg-yellow-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
             :disabled="!isFormValid"
           >
-            Continuer
+            {{ submitButton }}
           </button>
         </div>
       </form>
@@ -166,6 +175,26 @@
 const { t } = useI18n();
 const title = ref(t("submit.title"));
 const desc = ref(t("submit.desc"));
+const prenomLabel = ref(t("submit.form.prenom.label"));
+const prenomPlaceholder = ref(t("submit.form.prenom.placeholder"));
+const nomLabel = ref(t("submit.form.nom.label"));
+const nomPlaceholder = ref(t("submit.form.nom.placeholder"));
+const genreLabel = ref(t("submit.form.gender.label"));
+const genreOptionM = ref(t("submit.form.gender.options.male"));
+const genreOptionF = ref(t("submit.form.gender.options.female"));
+const genreOptionO = ref(t("submit.form.gender.options.other"));
+const countryLabel = ref(t("submit.form.country.label"));
+const countryPlaceholder = ref(t("submit.form.country.placeholder"));
+const emailLabel = ref(t("submit.form.email.label"));
+const emailPlaceholder = ref(t("submit.form.email.placeholder"));
+const emailError = ref(t("submit.form.email.error"));
+const phoneLabel = ref(t("submit.form.phone.label"));
+const phonePlaceholder = ref(t("submit.form.phone.placeholder"));
+const phoneError = ref(t("submit.form.phone.error"));
+const socialLabel = ref(t("submit.form.social.label"));
+const socialPlaceholder = ref(t("submit.form.social.placeholder"));
+const imageLabel = ref(t("submit.form.image.label"));
+const submitButton = ref(t("submit.form.submitButton.text"));
 
 const formData = reactive({
   prenom: "",
